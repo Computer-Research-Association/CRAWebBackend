2fbc51515b02edb166a022b7817472f731e5b859
chanhk-im <cfasd1875@gmail.com>
2025-01-21 14:38:18 +0900

    [CH] feat[#61]: submodule 변경 반영

--- src/main/java/com/handong/cra/crawebbackend/file/service/S3FileService.java
+++ src/main/java/com/handong/cra/crawebbackend/file/service/S3FileService.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface S3FileService {
 
     public String uploadFile(MultipartFile file, S3ImageCategory s3ImageCategory);
+    public String transferFile(String path, S3ImageCategory s3ImageCategory);
+    public List<String> transferFile(List<String> paths, S3ImageCategory s3ImageCategory);
 }
