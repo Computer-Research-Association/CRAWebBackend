e9afb98896c77a50598ec4265e60872b1c4af0ad
Whal3 <leehan416@gmail.com>
2025-02-12 10:58:57 +0900

    [HG]hotfix[#114]: 로그인시 기존 존재하는 리프레시 토큰 제거

--- src/main/java/com/handong/cra/crawebbackend/auth/service/AuthServiceImpl.java
+++ src/main/java/com/handong/cra/crawebbackend/auth/service/AuthServiceImpl.java
@@ -78,7 +78,7 @@ public class AuthServiceImpl implements AuthService {
         UserDetailDto userDetailDto = userService.getUserDetailByUsername(loginUserDto.getUsername());
 
         // 이미 존재하면 삭제
-        refreshTokenRepository.getRefreshTokenByUserId(userDetailDto.getId());
+        refreshTokenRepository.deleteAllByUserId(userDetailDto.getId());
 
         // 새로 생성
         TokenDto tokenDto = jwtTokenProvider.generateTokenByLogin(loginUserDto.getUsername());
