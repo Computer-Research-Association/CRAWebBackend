4eaf2729d67987ef1826be2605c1b04dc22fbac9
Whal3 <leehan416@gmail.com>
2025-02-25 16:37:49 +0900

    [HG]feat[#154]: 사용하지 않는 코드 제거

--- src/main/java/com/handong/cra/crawebbackend/board/controller/BoardController.java
+++ src/main/java/com/handong/cra/crawebbackend/board/controller/BoardController.java
@@ -12,7 +12,6 @@ import com.handong.cra.crawebbackend.board.dto.request.ReqUpdateBoardDto;
 import com.handong.cra.crawebbackend.board.dto.response.*;
 import com.handong.cra.crawebbackend.board.service.BoardService;
 import com.handong.cra.crawebbackend.exception.ErrorResponse;
-import com.handong.cra.crawebbackend.exception.board.BoardIllegalCategoryException;
 import com.handong.cra.crawebbackend.exception.board.BoardPageSizeLimitExceededException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -28,7 +27,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -64,16 +62,16 @@ public class BoardController {
     }
 
 
-    @Parameters(value = {
-            @Parameter(name = "category", description = "0 = NOTICE, 1 = ACADEMIC, 2 = HAVRUTA"),
-    })
-    @Operation(summary = "Category 로 전체 Board List 가져오기", description = "카테고리로 데이터의 리스트를 읽어옴.많은 데이터를 가져올 수 있기 때문에, 권장되지 않음. (page 사용 권장) [추후 admin만 접근 가능하게 할 예정]")
-    @ApiResponses(value = {
-
-            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ResListBoardDto.class))),
-            @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content()),
-            @ApiResponse(responseCode = "404", description = "category 정보 없음", content = @Content())
-    })
+//    @Parameters(value = {
+//            @Parameter(name = "category", description = "0 = NOTICE, 1 = ACADEMIC, 2 = HAVRUTA"),
+//    })
+//    @Operation(summary = "Category 로 전체 Board List 가져오기", description = "카테고리로 데이터의 리스트를 읽어옴.많은 데이터를 가져올 수 있기 때문에, 권장되지 않음. (page 사용 권장) [추후 admin만 접근 가능하게 할 예정]")
+//    @ApiResponses(value = {
+//
+//            @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = ResListBoardDto.class))),
+//            @ApiResponse(responseCode = "403", description = "권한 없음", content = @Content()),
+//            @ApiResponse(responseCode = "404", description = "category 정보 없음", content = @Content())
+//    })
 //
 //    @PreAuthorize("denyAll()")// 접속 불가
 //    @GetMapping("/{category}")
