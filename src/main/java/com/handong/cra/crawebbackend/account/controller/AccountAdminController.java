182546279a4ce259af2a3e3cf5a28ea3d4b013ef
Whal3 <leehan416@gmail.com>
2025-04-05 23:02:56 +0900

    [HG]docs[#215]: 간단한 refactor, 가능 문서화

--- src/main/java/com/handong/cra/crawebbackend/account/controller/AccountAdminController.java
+++ src/main/java/com/handong/cra/crawebbackend/account/controller/AccountAdminController.java
@@ -8,7 +8,6 @@ import com.handong.cra.crawebbackend.user.domain.UserRoleEnum;
 import com.handong.cra.crawebbackend.user.dto.PageUserDataDto;
 import com.handong.cra.crawebbackend.user.dto.response.ResPageUserDto;
 import com.handong.cra.crawebbackend.user.dto.response.ResUserAdminDetailDto;
-import com.handong.cra.crawebbackend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,64 +21,53 @@ import java.util.List;
 @Slf4j
 @RequestMapping("/api/admin/account")
 public class AccountAdminController {
-
     private final AccountService accountService;
-    private final UserRepository userRepository;
-
 
     @Value("${spring.data.page.MAX_PER_PAGE}")
     private Integer MAX_PAGE_SIZE;
 
     @GetMapping("/code/signup") // 가입 코드 생성
-    public ResponseEntity<List<ResCodeDto>> generateSignupCodes(@RequestParam Short length) {
+    public ResponseEntity<List<ResCodeDto>> generateSignupCodes(@RequestParam final Short length) {
         return ResponseEntity.ok(accountService.generateSignupCodes(length).stream().map(ResCodeDto::from).toList());
     }
 
-//    @GetMapping("/users")
-//    public ResponseEntity<List<ResUserDetailDto>> getUsersByEntranceYear(@RequestParam(required = false, defaultValue = "") String year, @RequestParam(required = false, defaultValue = "") String term) {
-//        if (year.isEmpty() && term.isEmpty()) throw new RuntimeException("test");
-//        return ResponseEntity.ok(accountService.getUsersByEntranceYear(year, term).stream().map(ResUserDetailDto::from).toList());
-//    }
-
-    @GetMapping("/users/page/{page}")
+    @GetMapping("/users/page/{page}") // 유저 리스트
     public ResponseEntity<ResPageUserDto> getPaginationUser(
-            @PathVariable Long page, // 0부터 시작
-            @RequestParam(required = false, defaultValue = "0") Integer perPage
+            @PathVariable final Long page, // 0부터 시작
+            @RequestParam(required = false, defaultValue = "0") final Integer perPage
     ) {
-        if (perPage > MAX_PAGE_SIZE) throw new PageSizeLimitExceededException();
+        if (perPage > MAX_PAGE_SIZE) {
+            throw new PageSizeLimitExceededException();
+        }
         return ResponseEntity.ok(ResPageUserDto.from(accountService.getPaginationUser(PageUserDataDto.builder().page(page).perPage(perPage).build())));
     }
 
-
-    @GetMapping("/users/search")
+    @GetMapping("/users/search") // 유저 검색
     public ResponseEntity<List<ResUserAdminDetailDto>> findUsersByName
-            (@RequestParam(required = false, defaultValue = "") String name) {
+            (@RequestParam(required = false, defaultValue = "") final String name) {
         return ResponseEntity.ok(accountService.findUsersByName(name).stream().map(ResUserAdminDetailDto::from).toList());
     }
 
-    @PutMapping("/users/auth")
-    public ResponseEntity<Void> addUserAuthById(@RequestParam Long userId, @RequestParam Integer authOption) {
+    @PutMapping("/users/auth") // 유저 권한 추가
+    public ResponseEntity<Void> addUserAuthById(@RequestParam final Long userId, @RequestParam final Integer authOption) {
         accountService.addUserAuthById(userId, UserRoleEnum.values()[authOption]);
         return ResponseEntity.ok().build();
     }
 
-
-    @DeleteMapping("/users/auth")
-    public ResponseEntity<Void> removeUserAuthById(@RequestParam Long userId, @RequestParam Integer authOption) {
+    @DeleteMapping("/users/auth") // 유저 권한 삭제
+    public ResponseEntity<Void> removeUserAuthById(@RequestParam final Long userId, @RequestParam final Integer authOption) {
         accountService.removeUserAuthById(userId, UserRoleEnum.values()[authOption]);
         return ResponseEntity.ok().build();
     }
 
-
-    @PutMapping("/users/active")
-    public ResponseEntity<Void> activeAccount(@RequestParam Long userId) {
+    @PutMapping("/users/active") // 휴면 계정 활성화
+    public ResponseEntity<Void> activeAccount(@RequestParam final Long userId) {
         accountService.activeAccount(userId);
         return ResponseEntity.ok().build();
     }
 
-
-    @DeleteMapping("/users/{userId}")
-    public ResponseEntity<Void> deleteUserById(@PathVariable Long userId) {
+    @DeleteMapping("/users/{userId}") // 유저 삭제
+    public ResponseEntity<Void> deleteUserById(@PathVariable final Long userId) {
         accountService.deleteUser(userId);
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/handong/cra/crawebbackend/account/controller/AccountController.java
+++ src/main/java/com/handong/cra/crawebbackend/account/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.handong.cra.crawebbackend.account.controller;
 
-
 import com.handong.cra.crawebbackend.account.domain.ManageTokenCategory;
 import com.handong.cra.crawebbackend.account.dto.FindUsernameDto;
 import com.handong.cra.crawebbackend.account.dto.req.ReqFindUsernameDto;
@@ -13,36 +12,36 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/account")
 public class AccountController {
-
     private final AccountService accountService;
 
     @GetMapping("/valid/username") // 유저 있는지 확인
-    public ResponseEntity<Void> validUsername(@RequestParam String username) {
-        if (accountService.validUsername(username))// 존재하지 않음.
+    public ResponseEntity<Void> validUsername(@RequestParam final String username) {
+        if (accountService.validUsername(username)) { // 존재하지 않음.
             return ResponseEntity.ok().build();
-        else return ResponseEntity.unprocessableEntity().build(); // 이미 존재함.
+        }
+        return ResponseEntity.unprocessableEntity().build(); // 이미 존재함.
     }
 
-    @GetMapping("/find/username")
-    public ResponseEntity<String> findUsername(ReqFindUsernameDto reqFindUsernameDto){
-        return ResponseEntity.ok(accountService.findUsername(FindUsernameDto.from(reqFindUsernameDto)));
-    }
-
-    @PostMapping("/password-change")
-    public ResponseEntity<Void> requestChangingPassword(@RequestParam String username) {
-        accountService.requestChangingPassword(username);
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/valid/email-request")
-    public ResponseEntity<Void> emailVerification(@RequestParam String email){
+    @PostMapping("/valid/email-request") // 이메일 확인용 전송
+    public ResponseEntity<Void> emailVerification(@RequestParam final String email){
         accountService.emailValidCheck(email);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/valid/email-code")
-    public ResponseEntity<Void> emailCodeCheck(@RequestParam String code) {
+    @PostMapping("/valid/email-code") // 이메일 검증
+    public ResponseEntity<Void> emailCodeCheck(@RequestParam final String code) {
         accountService.codeValidCheck(code, ManageTokenCategory.EMAIL_VALID);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/find/username") // 유저 찾기
+    public ResponseEntity<String> findUsername(final ReqFindUsernameDto reqFindUsernameDto){
+        return ResponseEntity.ok(accountService.findUsername(FindUsernameDto.from(reqFindUsernameDto)));
+    }
+
+    @PostMapping("/password-change") // 패스워드 변경
+    public ResponseEntity<Void> requestChangingPassword(@RequestParam final String username) {
+        accountService.requestChangingPassword(username);
         return ResponseEntity.ok().build();
     }
 }
